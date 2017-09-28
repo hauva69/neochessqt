55cc8528d60e1f3decf0b8362936d7a7a5902084
Richard F. Ashwell III <rashwell@localhost.localdomain>
2017-09-28 09:21:11 -0500

    Initialize App and Main Window

---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"os"
+	"runtime"
+
+	"github.com/therecipe/qt/core"
+	"github.com/therecipe/qt/widgets"
+)
+
+// VERSION of Application
+var VERSION = "0.0.1"
+
+func main() {
+	core.QCoreApplication_SetOrganizationDomain("neodevelop.net")
+	core.QCoreApplication_SetOrganizationName("NeoDevelop")
+	core.QCoreApplication_SetApplicationName("NeoChess")
+	core.QCoreApplication_SetApplicationVersion(VERSION)
+	qapp := widgets.NewQApplication(len(os.Args), os.Args)
+	qwin := widgets.NewQMainWindow(nil, 0)
+	if runtime.GOOS == "darwin" {
+		qwin.SetUnifiedTitleAndToolBarOnMac(true)
+	}
+	qwin.SetWindowTitle(core.QCoreApplication_ApplicationName())
+	desktop := qapp.Desktop()
+	screen := desktop.AvailableGeometry(-1)
+	qwin.Resize(core.NewQSize2(screen.Width()*80/100, screen.Height()*90/100))
+
+	var stylefile = core.NewQFile2(":qml/assets/basedark.css")
+	if stylefile.Open(core.QIODevice__ReadOnly) {
+		styledata := stylefile.ReadAll()
+		stylestr := styledata.ConstData()
+		qapp.SetStyleSheet(stylestr)
+	}
+
+	qwin.Show()
+	widgets.QApplication_Exec()
+
+}
