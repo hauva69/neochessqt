0a8ec9aa653f4e0f10c906b7b10f7bdd63671431
Richard F. Ashwell III <rashwell@gmail.com>
2017-10-01 14:12:41 -0500

    Added Color Picker to settings dialog

--- settings.go
+++ settings.go
@@ -15,14 +15,15 @@ import (
 
 // OptionType comment
 type OptionType struct {
-	Version string
-	Key     string
-	Label   string
-	Kind    string
-	Descr   string
-	Boolval bool
-	Strval  string
-	Intval  int
+	Version  string
+	Key      string
+	Label    string
+	Kind     string
+	Descr    string
+	Boolval  bool
+	Strval   string
+	Intval   int
+	Colorval *gui.QColor
 }
 
 // AppConfig comment
@@ -117,13 +118,14 @@ func initAppConfig(qapp *widgets.QApplication, qwin *widgets.QMainWindow) *AppCo
 	if !appconfig.Load() {
 		desktop := qapp.Desktop()
 		screenrect := desktop.AvailableGeometry(-1)
-		appconfig.Options = append(appconfig.Options, OptionType{"1.0.0", "LastWidth", "Last Application Width", "int", "Last width of application", false, "", int(screenrect.Width() * 80 / 100)})
-		appconfig.Options = append(appconfig.Options, OptionType{"1.0.0", "LastHeight", "Last Application Height", "int", "Last height of application", false, "", int(screenrect.Height() * 90 / 100)})
-		appconfig.Options = append(appconfig.Options, OptionType{"1.0.0", "ShowBoardLables", "Show Board Labels", "bool", "Show Algebraic labels on the edges of the board.", true, "", 0})
-		appconfig.Options = append(appconfig.Options, OptionType{"1.0.0", "ShowSquareLables", "Show Square Labels", "bool", "Show Algebraic labels on each square of board.", false, "", 0})
-		appconfig.Options = append(appconfig.Options, OptionType{"1.0.0", "ShowSideToMoveMarker", "Show Side to move", "bool", "Display indicator to side of the board.", true, "", 0})
-		appconfig.Options = append(appconfig.Options, OptionType{"1.0.0", "StyleFile", "Style File", "file", "CSS Files (*.css)", false, appconfig.Datadir + "/basedark.css", 0})
-		appconfig.Options = append(appconfig.Options, OptionType{"1.0.0", "PGNStyleFile", "PGN Style File", "file", "CSS Files (*.css)", false, appconfig.Datadir + "/pgntextstyle.css", 0})
+		appconfig.Options = append(appconfig.Options, OptionType{"1.0.0", "LastWidth", "Last Application Width", "int", "Last width of application", false, "", int(screenrect.Width() * 80 / 100), nil})
+		appconfig.Options = append(appconfig.Options, OptionType{"1.0.0", "LastHeight", "Last Application Height", "int", "Last height of application", false, "", int(screenrect.Height() * 90 / 100), nil})
+		appconfig.Options = append(appconfig.Options, OptionType{"1.0.0", "ShowBoardLables", "Show Board Labels", "bool", "Show Algebraic labels on the edges of the board.", true, "", 0, nil})
+		appconfig.Options = append(appconfig.Options, OptionType{"1.0.0", "ShowSquareLables", "Show Square Labels", "bool", "Show Algebraic labels on each square of board.", false, "", 0, nil})
+		appconfig.Options = append(appconfig.Options, OptionType{"1.0.0", "ShowSideToMoveMarker", "Show Side to move", "bool", "Display indicator to side of the board.", true, "", 0, nil})
+		appconfig.Options = append(appconfig.Options, OptionType{"1.0.0", "StyleFile", "Style File", "file", "CSS Files (*.css)", false, appconfig.Datadir + "/basedark.css", 0, nil})
+		appconfig.Options = append(appconfig.Options, OptionType{"1.0.0", "PGNStyleFile", "PGN Style File", "file", "CSS Files (*.css)", false, appconfig.Datadir + "/pgntextstyle.css", 0, nil})
+		appconfig.Options = append(appconfig.Options, OptionType{"1.0.0", "PossibleMove", "Possible Move Color", "color", "Possible Move Color", false, "", 0, gui.NewQColor3(8, 145, 17, 100)})
 	}
 
 	stylefile := appconfig.GetStrOption("StyleFile")
@@ -239,6 +241,20 @@ func (ac *AppConfig) EditConfig() {
 			widget := widgets.NewQWidget(nil, core.Qt__Widget)
 			widget.SetLayout(layout)
 			fbox.AddRow3(option.Label, widget)
+		case "color":
+			button := widgets.NewQPushButton2(option.Colorval.Name(), nil)
+			button.SetStyleSheet("QPushButton {background-color: " + option.Colorval.Name() + ";}")
+			button.ConnectClicked(func(checked bool) {
+				log.Info("Picked Color Picker")
+				colorDialog := widgets.NewQColorDialog2(option.Colorval, nil)
+				color := colorDialog.GetColor(option.Colorval, ac.Window, option.Label, widgets.QColorDialog__ShowAlphaChannel)
+				if color.IsValid() {
+					log.Infof("Picked Color: %s", color.Name())
+					button.SetStyleSheet("QPushButton {background-color: " + color.Name() + ";}")
+					button.SetText(color.Name())
+				}
+			})
+			fbox.AddRow3(option.Label, button)
 		case "int":
 			strint := strconv.Itoa(option.Intval)
 			item := widgets.NewQLineEdit2(strint, nil)
